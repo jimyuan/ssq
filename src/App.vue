3d12d5a30ec24adb6d8934980470b0e955bf06bd
Jim Yuan <aquarior@gmail.com>
2019-04-24 10:25:06 +0800

    精简代码，抛弃 axios 库, 改用原生

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,15 @@
     <picked-bets :bets="pickedBalls" v-if="validResults"></picked-bets>
     <bets-detail :bets="pickedBalls" v-if="validResults"></bets-detail>
     <div class="loading" v-if="loadingFlag">
-      <img src="loading.svg" alt="loading">
+      <svg viewBox="0 0 1024 1024" version="1.1" width="81" height="81">
+        <path d="M512 1024c-113.777778 0-221.866667-34.133333-307.2-102.4-39.822222-28.444444-45.511111-79.644444-17.066667-119.466667 28.444444-39.822222 79.644444-45.511111 119.466667-17.066666 56.888889 45.511111 130.844444 68.266667 204.8 68.266666 187.733333 0 341.333333-153.6 341.333333-341.333333s-153.6-341.333333-341.333333-341.333333-341.333333 153.6-341.333333 341.333333c0 45.511111-39.822222 85.333333-85.333334 85.333333S0 557.511111 0 512c0-284.444444 227.555556-512 512-512s512 227.555556 512 512-233.244444 512-512 512z"></path>
+      </svg>
       <span>数据加载中...</span>
     </div>
   </section>
 </template>
 
 <script>
-import axios from 'axios'
 import SsqInfo from '@/components/SsqInfo'
 import SsqAnaly from '@/components/SsqAnaly'
 import PickedBets from '@/components/PickedBets'
@@ -37,13 +38,18 @@ export default {
   },
   created () {
     this.loadingFlag = true
-    axios({
-      url: 'data.txt',
-      responseType: 'text'
-    }).then(req => {
-      this.ssq = req.data.replace(/\n$/, '').split('\n').map(item => item.split(','))
-      this.loadingFlag = false
-    })
+    const xhr = window.XMLHttpRequest
+      ? new XMLHttpRequest()
+      : window.ActiveXObject('microsoft.XMLHttp')
+    xhr.open('get', 'data.txt', true)
+    xhr.send()
+    xhr.onreadystatechange = () => {
+      const { readyState, status, responseText } = xhr
+      if (readyState === 4 && (status === 200 || status === 304)) {
+        this.ssq = responseText.replace(/\n$/, '').split('\n').map(item => item.split(','))
+        this.loadingFlag = false
+      }
+    }
   }
 }
 </script>
